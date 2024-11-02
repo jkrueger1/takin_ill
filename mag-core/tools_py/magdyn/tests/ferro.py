6563ea01406c2ea1d972d44f71024fd48669f24a
Tobias WEBER <tweber@ill.fr>
2024-11-02 10:33:38 +0100

    magdyn_py: cleanups

--- mag-core/tools_py/magdyn/tests/ferro.py
+++ mag-core/tools_py/magdyn/tests/ferro.py
@@ -8,53 +8,22 @@
 
 import numpy as np
 import numpy.linalg as la
-import matplotlib.pyplot as plt
 
 
-is_ferromagnetic = True  # choose ferromagnetic or antiferromagnetic 1d spin chain
-only_pos_E = True        # hide magnon annihilation?
 verbose_print = False    # print intermediate results
 
 
+#
+# debug output
+#
 def print_infos(str):
 	if verbose_print:
 		print(str)
 
 
 #
-# magnetic sites
-# "S": spin magnitude
-# "Sdir": spin direction
-#
-if is_ferromagnetic:
-	sites = [
-		{ "S" : 1., "Sdir" : [ 0, 0, 1 ] },
-	]
-else:  # antiferromagnetic
-	sites = [
-		{ "S" : 1., "Sdir" : [ 0, 0, 1 ] },
-		{ "S" : 1., "Sdir" : [ 0, 0, -1 ] },
-	]
-
-#
-# magnetic couplings
-# "sites": indices of the sites to couple
-# "J": (symmetric) exchange interaction
-# "DMI": (antisymmetric) Dzyaloshinskii-Moryia interaction
-# "dist": distance in rlu to the next unit cell for the coupling
-#
-if is_ferromagnetic:
-	couplings = [
-		{ "sites" : [ 0, 0 ], "J" : -1., "DMI" : [ 0, 0, 0 ], "dist" : [ 1, 0, 0 ] },
-	]
-else:  # antiferromagnetic
-	couplings = [
-		{ "sites" : [ 0, 1 ], "J" : 1., "DMI" : [ 0, 0, 0 ], "dist" : [ 0, 0, 0 ] },
-		{ "sites" : [ 1, 0 ], "J" : 1., "DMI" : [ 0, 0, 0 ], "dist" : [ 2, 0, 0 ] },
-	]
-
-
 # skew-symmetric (cross-product) matrix
+#
 def skew(vec):
 	return np.array([
 		[      0.,   vec[2],  -vec[1] ],
@@ -62,44 +31,50 @@ def skew(vec):
 		[  vec[1],  -vec[0],       0. ] ])
 
 
-# calculate spin rotations towards ferromagnetic order along [001]
-for site in sites:
-	zdir = np.array([ 0., 0., 1. ])
-	Sdir = np.array(site["Sdir"]) / la.norm(site["Sdir"])
-	rotaxis = np.array([ 0., 1., 0. ])
-	s = 0.
+#
+# calculate structure properties
+#
+def init(sites, couplings):
+	# calculate spin rotations towards ferromagnetic order along [001]
+	for site in sites:
+		zdir = np.array([ 0., 0., 1. ])
+		Sdir = np.array(site["Sdir"]) / la.norm(site["Sdir"])
+		rotaxis = np.array([ 0., 1., 0. ])
+		s = 0.
 
-	if np.allclose(Sdir, zdir):
-		# spin and z axis parallel
-		c = 1.
-	elif np.allclose(Sdir, -zdir):
-		# spin and z axis anti-parallel
-		c = -1.
-	else:
-		# sine and cosine of the angle between spin and z axis
-		rotaxis = np.cross(Sdir, zdir)
-		s = la.norm(rotaxis)
-		c = np.dot(Sdir, zdir)
-		rotaxis /= s
+		if np.allclose(Sdir, zdir):
+			# spin and z axis parallel
+			c = 1.
+		elif np.allclose(Sdir, -zdir):
+			# spin and z axis anti-parallel
+			c = -1.
+		else:
+			# sine and cosine of the angle between spin and z axis
+			rotaxis = np.cross(Sdir, zdir)
+			s = la.norm(rotaxis)
+			c = np.dot(Sdir, zdir)
+			rotaxis /= s
 
-	# rotation via rodrigues' formula, see (Arens 2015), p. 718 and p. 816
-	rot = (1. - c) * np.outer(rotaxis, rotaxis) + np.diag([ c, c, c ]) - skew(rotaxis)*s
-	site["u"] = rot[0, :] + 1j * rot[1, :]
-	site["v"] = rot[2, :]
+		# rotation via rodrigues' formula, see (Arens 2015), p. 718 and p. 816
+		rot = (1. - c) * np.outer(rotaxis, rotaxis) + np.diag([ c, c, c ]) - skew(rotaxis)*s
+		site["u"] = rot[0, :] + 1j * rot[1, :]
+		site["v"] = rot[2, :]
 
-	print_infos(np.dot(rot, Sdir))
-	print_infos("\nrot = \n%s\nu = %s\nv = %s" % (rot, site["u"], site["v"]))
-
-
-# calculate real interaction matrices
-for coupling in couplings:
-	J = coupling["J"]
-	coupling["J_real"] = np.diag([ J, J, J ]) + skew(coupling["DMI"])
-	print_infos("\nJ_real =\n%s" % coupling["J_real"])
+		print_infos(np.dot(rot, Sdir))
+		print_infos("\nrot = \n%s\nu = %s\nv = %s" % (rot, site["u"], site["v"]))
 
 
+	# calculate real interaction matrices
+	for coupling in couplings:
+		J = coupling["J"]
+		coupling["J_real"] = np.diag([ J, J, J ]) + skew(coupling["DMI"])
+		print_infos("\nJ_real =\n%s" % coupling["J_real"])
+
+
+#
 # get the energies of the dispersion at the momentum transfer Qvec
-def get_energies(Qvec):
+#
+def get_energies(Qvec, sites, couplings):
 	print_infos("\n\nQ = %s" % Qvec)
 
 	# fourier transform interaction matrices
@@ -164,27 +139,68 @@ def get_energies(Qvec):
 	return Es
 
 
-# calculate a single point
-#get_energies([0.2, 0., 0.])
-#exit(0)
+# test
+if __name__ == "__main__":
+	is_ferromagnetic = True  # choose ferromagnetic or antiferromagnetic 1d spin chain
+	only_pos_E = True        # hide magnon annihilation?
+
+	#
+	# set up magnetic sites and couplings:
+	#
+	#   - magnetic sites:
+	#     "S": spin magnitude
+	#     "Sdir": spin direction
+	#
+	#   - magnetic couplings:
+	#     "sites": indices of the sites to couple
+	#     "J": (symmetric) exchange interaction
+	#     "DMI": (antisymmetric) Dzyaloshinskii-Moryia interaction
+	#     "dist": distance in rlu to the next unit cell for the coupling
+	#
+	if is_ferromagnetic:
+		sites = [
+			{ "S" : 1., "Sdir" : [ 0, 0, 1 ] },
+		]
+
+		couplings = [
+			{ "sites" : [ 0, 0 ], "J" : -1., "DMI" : [ 0, 0, 0 ], "dist" : [ 1, 0, 0 ] },
+		]
+	else:  # antiferromagnetic
+		sites = [
+			{ "S" : 1., "Sdir" : [ 0, 0, 1 ] },
+			{ "S" : 1., "Sdir" : [ 0, 0, -1 ] },
+		]
+
+		couplings = [
+			{ "sites" : [ 0, 1 ], "J" : 1., "DMI" : [ 0, 0, 0 ], "dist" : [ 0, 0, 0 ] },
+			{ "sites" : [ 1, 0 ], "J" : 1., "DMI" : [ 0, 0, 0 ], "dist" : [ 2, 0, 0 ] },
+		]
+
+	init(sites, couplings)
 
 
-# plot a dispersion branch
-hs = []
-Es = []
-for h in np.linspace(-1, 1, 1024):
-	try:
-		Qvec = np.array([ h, 0, 0 ])
-		for E in get_energies(Qvec):
-			if only_pos_E and E < 0.:
-				continue
-			hs.append(h)
-			Es.append(E)
-	except la.LinAlgError:
-		pass
+	# calculate a single point
+	#get_energies([0.2, 0., 0.])
+	#exit(0)
 
-plt.plot()
-plt.xlabel("h (rlu)")
-plt.ylabel("E (meV)")
-plt.scatter(hs, Es, marker = '.')
-plt.show()
+	# plot a dispersion branch
+	import matplotlib.pyplot as plt
+
+	hs = []
+	Es = []
+	for h in np.linspace(-1, 1, 1024):
+		try:
+			Qvec = np.array([ h, 0, 0 ])
+			for E in get_energies(Qvec, sites, couplings):
+				if only_pos_E and E < 0.:
+					continue
+				hs.append(h)
+				Es.append(E)
+		except la.LinAlgError:
+			pass
+
+	plt.plot()
+	plt.xlabel("h (rlu)")
+	plt.ylabel("E (meV)")
+	plt.scatter(hs, Es, marker = '.')
+	plt.show()
